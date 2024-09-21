7d94221e97ace6d06b48781bda470f078cbbef71
rhythmitaliya <ritaliya2003@gmail.com>
2024-09-21 23:51:19 +0530

    21-09-2024

--- app/server.js
+++ app/server.js
@@ -1,35 +1,40 @@
 'use strict';
-// load configs at first 
-const configs       = require('./configs');
-let port            = process.env.PORT || 3000;
 
-// import the npm and node modules here 
-const express       = require('express');
-const bodyParser    = require('body-parser');
-const path          = require('path');
-const ejs           = require('ejs');
+// Load configs at first 
+const configs = require('./configs');
+let port = process.env.PORT || 3000;
 
-// create the app
+// Import npm and node modules
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const ejs = require('ejs');
+
+// Create the app
 const app = express();
 
-// middlewares here
-app.use(bodyParser.urlencoded( {extended : true}));
+// Middlewares here
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname + '/_public')));
+app.use(express.static(path.join(__dirname, '/_public')));
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname + '/views'));
+app.set('views', path.join(__dirname, '/views'));
 
-// custom modules 
-const mongoose      = require('./db');
-const publicRoutes  = require('./routes/public')(app);
+// Custom modules
+const mongoose = require('./db');
+const publicRoutes = require('./routes/public')(app);
 
+// Routes
 app.get('/', (req, res) => {
     res.render('index');
 });
 
-app.listen(port, () => {
-    console.log('app started and listening to port ', port);
-})
+// Only listen on a port in a local or non-serverless environment
+if (!process.env.VERCEL) {
+    app.listen(port, () => {
+        console.log('App started and listening to port', port);
+    });
+}
 
-// we need to export the 'app' for testing purpose
-module.exports = {app};
+// Export the app directly for Vercel
+module.exports = app;
