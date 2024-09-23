26708739fdf8c9bf722d465f44db03d95119e79f
rhythmitaliya <ritaliya2003@gmail.com>
2024-09-23 21:40:09 +0530

    23-09-2024

--- app/configs/index.js
+++ app/configs/index.js
@@ -1,3 +1,26 @@
+// 'use strict';
+
+// // Load environment variables from .env file
+// require('dotenv').config();
+
+// // Determine the environment
+// let env = process.env.NODE_ENV || 'development';
+// let port = process.env.PORT || 3000;
+
+// console.log('****Working environment : ', env);
+
+// // Set database URI based on environment
+// if (env === 'development') {
+//     process.env.MONGODB_URI = process.env.MONGODB_URI_DEVELOPMENT;
+// } else if (env === 'test') {
+//     process.env.MONGODB_URI = process.env.MONGODB_URI_TEST;
+// } else {
+//     throw new Error('Unknown environment: ' + env);
+// }
+
+// // Optional: Log the selected MongoDB URI
+// console.log('Using MongoDB URI:', process.env.MONGODB_URI);
+
 'use strict';
 
 // Load environment variables from .env file
@@ -14,6 +37,8 @@ if (env === 'development') {
     process.env.MONGODB_URI = process.env.MONGODB_URI_DEVELOPMENT;
 } else if (env === 'test') {
     process.env.MONGODB_URI = process.env.MONGODB_URI_TEST;
+} else if (env === 'production') {
+    process.env.MONGODB_URI = process.env.MONGODB_URI_PRODUCTION; // Add this line
 } else {
     throw new Error('Unknown environment: ' + env);
 }
